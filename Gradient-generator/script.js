3ac7b4a9ef2da27a5ee4ef35b9296779e629bdf4
SebDevs <seb.heinonen@gmail.com>
2018-12-13 20:22:46 +0200

    Gradient Js

---
+++ Gradient-generator/script.js
@@ -0,0 +1,16 @@
+var css = document.querySelector("h3");
+var firstcolor = document.getElementById("first");
+var secondcolor = document.getElementById("second");
+var body = document.getElementById("base");
+var printarea = document.getElementById("result");
+
+function setGradient(){
+    body.style.background = "linear-gradient(to right, "+ firstcolor.value +", " + secondcolor.value + ")"; 
+    printarea.innerHTML = "linear-gradient(to right, "+ firstcolor.value +", " + secondcolor.value + ")";
+}
+
+firstcolor.addEventListener("input", setGradient,);
+secondcolor.addEventListener("input", setGradient);
+
+
+
